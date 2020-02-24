98ece5c6608c73d1dceee4180104cd5066bfceff
Zoltan Kohalmy <zoltankohalmy@gmail.com>
2020-02-24 19:57:47 +0100

    Make checklist description optional

--- code/backend/checklists/models.py
+++ code/backend/checklists/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class CheckList(models.Model):
     title = models.CharField(max_length=255)
-    description = models.CharField(max_length=255)
+    description = models.CharField(max_length=255, default="", blank=True)
     latest_run = models.ForeignKey(
         'CheckListRun',
         blank=True,
