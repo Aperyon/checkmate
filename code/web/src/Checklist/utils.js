089013b124ec64564d1483b7d75eb5e29ecc3a88
Zoltan Kohalmy <zoltankohalmy@gmail.com>
2020-03-13 23:43:28 +0100

    Fix Adding new fields when all all existing items are filled

--- code/web/src/Checklist/utils.js
+++ code/web/src/Checklist/utils.js
@@ -2,14 +2,19 @@ import _ from 'lodash';
 
 
 export function handleItemsChange(values, append) {
+  console.log('Checking!!!!')
+  console.log(values)
   if (shouldAddExtraItem(values)) {
+    console.log('Should add yes')
     append()
+  } else {
+    console.log('Dont add')
   }
 }
 
 
 export function shouldAddExtraItem(values) {
-  const itemFieldKeys = Object.keys(values).filter(fieldName => fieldName.indexOf('items[') === 0)
+  const itemFieldKeys = Object.keys(values).filter(fieldName => fieldName.indexOf('items[') === 0 && fieldName.indexOf('.text') > -1)
   const itemFieldValues = itemFieldKeys.map(key => values[key])
   return _.every(itemFieldValues.map(val => val.trim() !== ""))
 }
